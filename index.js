18c0ce21fe9a8c25ca629bf31cc22e5ea036083c
Odilson Woodly Italis <italisodilsonwoodly@hotmail.com>
2024-06-30 23:34:38 -0400

    Configure express

--- index.js
+++ index.js
@@ -1,57 +1,21 @@
 const express = require("express");
-const http = require("http");
-const https = require("https");
-
 const app = express();
 
-app.get("/api/hello", (req, res) => {
-  const visitorName = req.query.visitor_name;
-  const clientIp =
-    req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+function logOriginalUrl(req, res, next) {
+  console.log("Request URL:", req.originalUrl);
+  next();
+}
 
-  // Get location based on IP address
-  http
-    .get(`http://ip-api.com/json/${clientIp}`, (geoRes) => {
-      let geoData = "";
+function logMethod(req, res, next) {
+  console.log("Request Type:", req.method);
+  next();
+}
 
-      geoRes.on("data", (chunk) => {
-        geoData += chunk;
-      });
-
-      geoRes.on("end", () => {
-        const location = JSON.parse(geoData).city;
-
-        // Get weather data based on location
-        https
-          .get(
-            `https://api.weatherapi.com/v1/current.json?key=YOUR_API_KEY&q=${location}`,
-            (weatherRes) => {
-              let weatherData = "";
-
-              weatherRes.on("data", (chunk) => {
-                weatherData += chunk;
-              });
-
-              weatherRes.on("end", () => {
-                const temperature = JSON.parse(weatherData).current.temp_c;
-
-                res.json({
-                  client_ip: clientIp,
-                  location: location,
-                  greeting: `Hello, ${visitorName}!, the temperature is ${temperature} degrees Celsius in ${location}`,
-                });
-              });
-            }
-          )
-          .on("error", (e) => {
-            res.status(500).send("Error occurred while fetching weather data");
-          });
-      });
-    })
-    .on("error", (e) => {
-      res.status(500).send("Error occurred while fetching location data");
-    });
+const logStuff = [logOriginalUrl, logMethod];
+app.get("/user/:id", logStuff, (req, res, next) => {
+  res.send("User Info");
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
